5eb0c738080f368cebb7ac576d77682b1035c5cc
Tatiana <tlavrova230@gmail.com>
2022-07-18 17:07:18 +0200

    Viewing details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HEROES} from "../mock-heroes";
+import {Hero} from "../hero";
 
 @Component({
   selector: 'app-heroes',
@@ -8,21 +9,33 @@ import {HEROES} from "../mock-heroes";
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes">
-        <button type="button">
+        <button type="button" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
           <span class="badge">{{hero.id}}</span>
           <span class="name">{{hero.name}}</span>
         </button>
       </li>
     </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name | uppercase}} Details</h2>
+      <div>id: {{selectedHero.id}}</div>
+      <div>
+        <label for="hero-name">Hero name: </label>
+        <input id="hero-name" [(ngModel)]="selectedHero.name" placeholder="name">
+      </div>
+    </div>
   `
 })
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES
+  selectedHero?: Hero
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero) {
+    this.selectedHero = hero
+  }
 }
