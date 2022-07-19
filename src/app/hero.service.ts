23484bf7bee63eb3e90e51bfa5dc0d8b1760909c
Tatiana <tlavrova230@gmail.com>
2022-07-19 10:12:26 +0200

    Observable data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HEROES} from "./mock-heroes";
 import {Hero} from "./hero";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES)
+    return heroes;
   }
 }
